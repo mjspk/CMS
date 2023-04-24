d85360ec7fbdc6f2f5f72cc1e0801d8387b0eb92
Mohammed Alhajjaj <mjspk@outlook.com>
2023-04-24 14:51:14 -0600

    alignment fixed

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,24 +1,21 @@
 <template>
     <div class="about container">
         <h2>About CMS</h2>
-        <p>
-            The Contact Management System is a web application built with Node.js,
-            Express, MySQL, and Vue.js. It allows users to manage their contacts,
-            including adding, updating, deleting, and searching for contacts. The
-            application also includes a feature to send an email to a contact.
-        </p>
-        <p>
-            The back-end API is built with Node.js and
-            Express, and connects to a
-            MySQL database to store contact information. The front-end is built with
-            Vue.js and communicates with the back-end API to display and manipulate
-            contact data
-        </p>
-        <p>
-            The Contact Management System is deployed with Nginx on Azure, allowing
-            users to access the application from anywhere with an internet
-            connection.
-        </p>
+        <p>The Contact Management System is a web application that enables users to manage their contacts, including adding,
+            updating, deleting, and searching for contacts. The application also includes a feature to send an email to a
+            contact.</p>
+        <p>The system is built using a combination of Node.js, Express, MySQL, and Vue.js, providing a robust and scalable
+            solution for managing contact information.</p>
+        <ul>
+            <li>The back-end API is built with Node.js and Express, connecting to a MySQL database to store contact
+                information.</li>
+            <li>The front-end is built with Vue.js, providing a dynamic and user-friendly interface for displaying and
+                manipulating contact data.</li>
+            <li>The application is deployed with Nginx on Azure, ensuring reliable and secure access from anywhere with an
+                internet connection.</li>
+        </ul>
+        <p>The Contact Management System is an efficient and effective tool for managing contacts, providing users with the
+            ability to easily organize and communicate with their contacts.</p>
     </div>
 </template>
 
@@ -33,18 +30,22 @@ export default class About extends Vue {
 <style scoped>
 .about {
     padding: 3rem;
+    text-align: left;
+
 }
 
 .about h2 {
     font-size: 2rem;
     font-weight: 400;
     margin-bottom: 1rem;
+    text-align: center;
+
+
 }
 
 .about p {
     font-size: 1.1rem;
     font-weight: 300;
     line-height: 1.5;
-    text-align: justify;
 }
 </style>
